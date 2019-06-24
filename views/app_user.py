81ee7f35875a9a5f2598ea8fbf89209679f10547
disen-xaqf <610039018@qq.com>
2019-06-24 11:48:58 +0800

    update app_user regist view

--- views/app_user.py
+++ views/app_user.py
@@ -6,7 +6,7 @@ from dao.user_dao import UserDao
 blue = Blueprint('user_api', __name__)
 
 
-@blue.route('/regist/', methods=('POST', ))
+@blue.route('/regist/', methods=('POST',))
 def user_regist():
     # 前端请求的Content-Type: application/json
     req_data = None
@@ -14,25 +14,31 @@ def user_regist():
     if request.headers['Content-Type'].startswith('application/json'):
         req_data = request.get_json()
 
+    if req_data is None:
+        api_logger.warn('%s 请求参数未上传-json' % request.remote_addr)
+        return jsonify({
+            'code': 9000,
+            'msg': '请上传json数据，且参数必须按api接口标准给定'
+        })
+
     api_logger.debug(req_data)
 
     # 验证上传的必须的数据是否存在
-    if all(req_data.get('user_name', False),
-           req_data.get('auth_string', False),
-           req_data.get('nick_name', False),
-           req_data.get('phone', False)):
-
+    if all((req_data.get('user_name', False),
+            req_data.get('auth_string', False),
+            req_data.get('nick_name', False),
+            req_data.get('phone', False))):
         dao = UserDao()
         dao.save(**req_data)
 
     return jsonify({
-       'code': 8000,
+        'code': 8000,
         'msg': 'ok',
         'data': req_data
     })
 
 
-@blue.route('/login', methods=('GET', ))
+@blue.route('/login', methods=('GET',))
 def user_login():
     api_logger.debug('user login get action!')
     return "<html><head><title>Login Page</title></head><body>Hi, Disen</body></html>"
