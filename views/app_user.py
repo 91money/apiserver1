0ebaaa8a8ba004d9ceeb959789386195d4acac1b
disen <610039018@qq.com>
2019-06-24 10:19:04 +0800

    第一次初始化api-server

---
+++ views/app_user.py
@@ -0,0 +1,22 @@
+from flask import Blueprint
+from flask import request, jsonify
+from logger import api_logger
+
+blue = Blueprint('user_api', __name__)
+
+
+@blue.route('/regist/', methods=('POST', ))
+def user_regist():
+    # 前端请求的Content-Type: application/json
+    req_data = None
+    api_logger.info(request.headers)
+    if request.headers['Content-Type'].startswith('application/json'):
+        req_data = request.get_json()
+
+    api_logger.debug(req_data)
+
+    return jsonify({
+       'code': 8000,
+        'msg': 'ok',
+        'data': req_data
+    })
